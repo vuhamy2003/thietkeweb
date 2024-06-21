e860c6410ea5077533027fba9020be0ece6bb43b
My Vũ <165868043+miyukizumy02@users.noreply.github.com>
2024-06-21 16:53:42 +0700

    21/06/2024

---
+++ js/main.js
@@ -0,0 +1,38 @@
+function showHotline() {
+    var hotlineNumber = document.getElementById('hotline-number');
+    hotlineNumber.style.display = 'inline';
+
+    var hotlineLink = document.getElementById('hotline-link');
+    hotlineLink.style.display = 'none';
+}
+
+function showEmail() {
+    var emailText = document.getElementById('email-text');
+    emailText.style.display = 'inline'; // Hiển thị địa chỉ email
+
+    var emailLink = document.getElementById('email-link');
+    emailLink.style.display = 'none'; // Ẩn biểu tượng email
+}
+
+// Hàm hiệu ứng khi di chuột qua
+function hoverEffect(element) {
+    element.style.transform = 'scale(1.2)'; // Phóng to hình ảnh khi di chuột qua
+}
+
+// Hàm hiệu ứng bình thường khi di chuột rời khỏi
+function normalEffect(element) {
+    element.style.transform = 'scale(1)'; // Trở về kích thước bình thường khi di chuột rời khỏi
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const dropdown = document.querySelector('.dropdown');
+    const dropdownContent = document.querySelector('.dropdown-content');
+
+    dropdown.addEventListener('mouseenter', function() {
+        dropdownContent.style.display = 'block';
+    });
+
+    dropdown.addEventListener('mouseleave', function() {
+        dropdownContent.style.display = 'none';
+    });
+});
